6ee2e22adfd9a9fe7a83a4d2eef41ad61fa25661
Thomas DeBellis <thomas.debellis@gmail.com>
2022-03-09 23:01:38 -0500

    3-9-22 js calculated form

--- Form.js
+++ Form.js
@@ -1,14 +1,6 @@
 function calculateTotal() {
-    let total = $("#qty").val() * $("#po-price").val();
-    $("#total_value").text(total);
+    let qty = parseFloat(document.getElementById('qty').value); // I'm assuming this isn't #qty because it is searching for the specific ID //
+    let poPrice = parseFloat(document.getElementById('po-price').value);
+    document.getElementById('total_value').value = qty * poPrice;
 }
-$(function() {
-        $(".qty").on("change keyup", calculateTotal)
-    })
-    // $(function() {
-    //     $(".po-price").on("change keyup", calculateTotal)
-    // })
-
-// I need to take the module on JS to understand why this isn 't working.
-
-console.log(total);
+// I need to work on trouble shooting, I was not able to understand that it wouldn't work due to the "calculate" button being a div instead of a button and also that the getElementById needed to be a string 'qty' vs. #qty. This makes sense because it is already looking for an ID instead of CSS needing to be told to look for #ID//
